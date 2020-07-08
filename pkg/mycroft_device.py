546051c45c501170f27cc72f0b852117598f9568
jamesmf <jamesmfrick@gmail.com>
2020-07-08 00:21:38 -0400

    fix boot

--- pkg/mycroft_device.py
+++ pkg/mycroft_device.py
@@ -1,6 +1,8 @@
 from gateway_addon import Device, Property
 import threading
 import time
+import json
+import websocket
 
 
 class MycroftDevice(Device):
@@ -8,7 +10,7 @@ class MycroftDevice(Device):
 
     msg_template = '{"type": "{msg_type}", "data": {msg_data_str}}'
 
-    def __init__(self, adapter, _id, device_ip):
+    def __init__(self, adapter, _id, message_types, device_ip):
         """
         Initialize the object.
         adapter -- the Adapter managing this device
@@ -16,6 +18,7 @@ class MycroftDevice(Device):
         device_ip -- ip of the mycroft on your gateway's local network
         """
         Device.__init__(self, adapter, _id)
+        self.message_types = message_types
         self._type = ["SmartSpeaker"]
         self.message = self.format_message("mozilla-iot.initialized", {})
 
@@ -23,14 +26,14 @@ class MycroftDevice(Device):
         self.name = "mycroft"
         self.queue = []
 
-        self.ws_reader = websocket.create_connection(f"ws://{host}:8181/core")
-        self.ws_sender = websocket.create_connection(f"ws://{host}:8181/core")
+        self.ws_reader = websocket.create_connection(f"ws://{device_ip}:8181/core")
+        self.ws_sender = websocket.create_connection(f"ws://{device_ip}:8181/core")
 
         send_thread = threading.Thread(target=self.send)
         send_thread.daemon = True
         send_thread.start()
 
-        read_thread = threading.Thread(target.self.read)
+        read_thread = threading.Thread(target=self.read)
         read_thread.daemon = True
         read_thread.start()
 
@@ -42,15 +45,15 @@ class MycroftDevice(Device):
 
     def send(self):
         if self.message is not None:
-            self.ws_sender.send(message)
+            self.ws_sender.send(self.message)
             self.message = None
 
     def read(self):
         r = self.ws_reader.recv()
         self.queue.append(r)
-        if len(queue) > 100:
+        if len(self.queue) > 100:
             self.queue = self.queue[:100]
 
-    def format_message(type_, data):
+    def format_message(self, type_, data):
         data = json.dumps(data)
         return self.msg_template.format(msg_type=type_, msg_data_str=data)
